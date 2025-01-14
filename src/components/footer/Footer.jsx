a8d4cbe8fc2e00116a2886d8a928fcab9d1158c9
Ananya Lamba <ananyalamba2002@gmail.com>
2025-01-14 13:19:00 +0530

    nav and footer implementation

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,108 @@
+
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="container">
+       <div className="top">
+        Back To Top
+       </div>
+       <div className="infromation">
+        <div className="Us">
+            <h4>Get To Know Us</h4>
+            <ul>
+                <li>
+                    About Amazon
+                </li>
+                <li>
+                    Careers
+                </li>
+                <li>
+                    Press Release
+                </li>
+                <li>
+                    Amazon Science
+                </li>
+            </ul>
+        </div>
+        <div className="Contect">
+            <h4> Contect Us With</h4>
+            <ul>
+                <li>
+                    Facebook 
+                </li>
+                <li>
+                    Twitter
+                </li>
+                <li>
+                    Instagram
+                </li>
+            </ul>
+        </div>
+        <div className="Money">
+            <h4>Make money with us</h4>
+            <ul>
+                <li>
+                    sell on Amazon
+                </li>
+                <li>
+                    Sell on Amazon Accelerator
+                </li>
+                <li>
+                    Protect and Build Your Brand
+                </li>
+                <li>
+                    Amazon Global Selling
+                </li>
+                <li>
+                    Supply to Amazon
+                </li>
+                <li>
+                    Become an Affiliate
+                </li>
+                <li>
+                    Fulfilment by Amazon
+                </li>
+                <li>
+                    Advertise Your Products
+                </li>
+                <li>
+                    Amazon Pay on Merchants
+                </li>
+            </ul>
+        </div>
+        <div className="help">
+            <h4>Let us Hepl You</h4>
+            <ul>
+                <li>
+                    Your Account 
+                </li>
+                <li>
+                    Return Center
+                </li>
+                <li>
+                    Recall and Product Safety Alerts
+                </li>
+                <li>
+                    100% Purchase Protection
+                </li>
+                <li>
+                    Amazon Download
+                </li>
+                <li>
+                    Help
+                </li>
+            </ul>
+        </div>
+       </div>
+       <hr />
+       <div className="images">
+        <h4>Amazon</h4>
+        <p>language</p>
+        <p>country</p>
+       </div>
+
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/footer/Footer_below.jsx
@@ -0,0 +1,26 @@
+import "./Footer.css"
+const Footer_Below = () => {
+    return ( 
+        <div className="main">
+            <div className="first">
+                <div className="above">
+                    <h2>AbeBooks</h2>
+                    <p>Books, Arts & collectibles</p>
+                </div>
+                <div className="below">
+                    <h2>Shopbop</h2>
+                    <p>Designer fashion Brands</p>
+                </div>
+            </div>
+            <div className="second">
+                <div className="second_above">
+                    <h2>Amazon Web Service</h2>
+                    <p>Scalable Cloud </p>
+                    <p>Computing Services</p>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Footer_Below;
